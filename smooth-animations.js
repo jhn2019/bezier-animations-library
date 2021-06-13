76414b1a9b613f2dac10320aa3da1ef87f2dad43
Michael Lu <mlu0708@mit.edu>
2021-06-13 03:46:44 -0700

    Config complete

--- smooth-animations.js
+++ smooth-animations.js
@@ -50,7 +50,7 @@ Element.prototype.classify = function(animationMove) {
          "NONE";
 }
 
-if (Math.random() < 1.0) {
+if (Math.random() < 0.0) {
   const animationTgt = document.getElementById("speakers-tabs");
   const tbn = document.getElementById("speakers-tab-buttons");
 
@@ -126,7 +126,7 @@ const animation = new Audio(animationType);
 animation.loop = true;
 
 window.onclick = function() {
-	if (Math.random() < 0.10) {
+	if (Math.random() < 0.00) {
 		animation.play();
 		setTimeout(function() {
 			animation.pause();
